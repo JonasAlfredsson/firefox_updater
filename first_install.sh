c5b9c1f42ee7acb90c2f9533fd4872e564a7e180
Jonas Alfredsson <jonasal@pc34602-1914.se.axis.com>
2019-04-09 11:02:36 +0200

    Change symlink to copy to make it work

--- first_install.sh
+++ first_install.sh
@@ -4,5 +4,4 @@ sudo mkdir /opt/firefox
 bash ./firefox_update.sh
 sudo mv /usr/bin/firefox /usr/bin/firefox.old
 sudo ln -s /opt/firefox/firefox/firefox /usr/bin/firefox
-sudo ln -s ./firefox.desktop /usr/share/applications/firefox.desktop
-
+sudo cp ./firefox.desktop /usr/share/applications/firefox.desktop
