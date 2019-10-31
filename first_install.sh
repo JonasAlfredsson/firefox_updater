54c83ba081a409c6fcc36d1e361e3dcf1956d63e
jonas <jonas.alfredsson@protonmail.com>
2019-10-31 22:14:51 +0100

    Minor change to the order of execution

--- first_install.sh
+++ first_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex
-sudo mkdir /opt/firefox
-bash ./firefox_update.sh
+sudo mkdir -p /opt/firefox
 sudo mv /usr/bin/firefox /usr/bin/firefox.old
+bash ./firefox_update.sh
 sudo ln -s /opt/firefox/firefox/firefox /usr/bin/firefox
 sudo cp ./firefox.desktop /usr/share/applications/firefox.desktop
