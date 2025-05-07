fbee95e6890763c9d9ffd558ed772ee7c23bfdb2
Jonas Alfredsson <jonasal@axis.com>
2025-05-07 10:16:07 +0200

    Handle new xz compression format
    It appears like Firefox switched from the bzip2 to the xz compression
    format, and our tar command fails to decompress it since we explicitly
    say that it should be bzip2. Change so that we let tar figure it out
    by itself.

--- firefox_update.sh
+++ firefox_update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -ex
 wget -O ~/Downloads/FirefoxSetup.tar.bz2 "https://download.mozilla.org/?product=firefox-latest&os=linux64&lang=en-US"
-sudo tar xjf ~/Downloads/FirefoxSetup.tar.bz2 -C /opt/firefox/
+sudo tar xf ~/Downloads/FirefoxSetup.tar.bz2 -C /opt/firefox/
 rm ~/Downloads/FirefoxSetup.tar.bz2
