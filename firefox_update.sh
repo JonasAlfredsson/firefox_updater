3ba83a0619a79aba34cc62214f7bb265adf0061a
Jonas Alfredsson <jonasal@axis.com>
2025-05-07 10:21:27 +0200

    Use a "trap" for cleanup
    If the download fail we won't continue the script and we might end up
    with a lot of empty temporary files. The "trap" command will trigger
    upon script exit and thus remove the temporary file as the very last
    step.

--- firefox_update.sh
+++ firefox_update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -eux
 tmpFile="$(mktemp)"
+trap "rm -v '${tmpFile}'" EXIT
 wget -O "${tmpFile}" "https://download.mozilla.org/?product=firefox-latest&os=linux64&lang=en-US"
 sudo tar xfv "${tmpFile}" -C /opt/firefox/
-rm "${tmpFile}"
